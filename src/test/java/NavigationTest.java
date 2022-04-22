e32313ab42e489ee9ea62595393a6202ed2cd15c
Sarath Kumar Soundararajan <sarath13ecb25@gmail.com>
2022-04-22 16:34:10 +0530

    Add files via upload

---
+++ src/test/java/NavigationTest.java
@@ -0,0 +1,29 @@
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+
+public class NavigationTest {
+	WebDriver driver;
+
+	@Test
+	public void navigateToUrl() {
+		driver.get("https://phptravels.com/demo/");
+		System.out.println(driver.getTitle());
+		driver.manage().window().maximize();
+
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+		System.setProperty("webdriver.chrome.driver", "./src/test/resources/drivers/chromedriver1.exe");
+		driver = new ChromeDriver();
+
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+	}
+
+}
